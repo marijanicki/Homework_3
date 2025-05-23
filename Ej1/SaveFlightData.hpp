018855094d820d4a348d87fa1f64eff3c55fd9b4
María Paz Janicki <mjanicki@udesa.edu.ar>
2025-05-23 15:27:42 -0300

    planteo de uml completo, faltan las implementaciones

--- Ej1/SaveFlightData.hpp
+++ Ej1/SaveFlightData.hpp
@@ -3,3 +3,14 @@
 #include "Presion_pos.hpp"
 
 using namespace std;
+
+class SaveFlightData{
+    public:
+        unique_ptr<Posicion> posicion;
+        unique_ptr<Presion> presion;
+
+        SaveFlightData(const Posicion& p, const Presion& q);
+        void serializar(ofstream& out);
+        void deserializar(ifstream& in);
+        void imprimir();
+};
---
+++ Ej1/main.cpp
@@ -0,0 +1,11 @@
+#include "Imediciones.hpp"
+#include "MedicionBase.hpp"
+#include "Presion_pos.hpp"
+#include "SaveFlightData.hpp"
+
+using namespace std;
+
+int main(){
+    Posicion posicion(-34.6f, -58.4f, 950.0f, 5.3);
+    Presion presion(101.3f, 5.8f, 6.1f);
+}
