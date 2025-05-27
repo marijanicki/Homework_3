435c93ca3786fe02b31c1a07b40530759d9dd067
María Paz Janicki <mjanicki@udesa.edu.ar>
2025-05-27 09:43:48 -0300

    clase 1 ej3

--- Ej3/Clase1.hpp
+++ Ej3/Clase1.hpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <type_traits>
 using namespace std;
+//double, string, vector int
+
+using vect = vector<int>;
 
 template<typename T>
 class Clase1{
     private:
         vector<T> datos;
     public:
+        Clase1(){};
         void add(T new_data){datos.push_back(new_data);}
-        void show(){
-            for(auto val : datos){
-                cout <<val<<endl;}
+        vector<string> procesar(){
+            if constexpr(is_floating_point<T>::value){
+               vector<string> str_vect;
+                for(auto val: datos){
+                    str_vect.push_back(to_string(val));
+                }
+                return str_vect;
+            }
+            else if constexpr(is_same<T, string>::value){
+                return datos;
+            }
+            else if constexpr(is_same<T,vect>::value){
+                vector<string> str_vect;
+                for(auto val: datos){
+                    str_vect.push_back(to_string(val));  
+                }
+                 return str_vect;
+            }
+            
         }
+      
 };
--- Ej3/main.cpp
+++ Ej3/main.cpp
@@ -1,12 +1,30 @@
 #include "Clase1.hpp"
 #include <iostream>
-
+#include <typeinfo>
 using namespace std;
 
 int main(){
-    Clase1<int> ej;
-    ej.add(2);
-    ej.show();
-    ej.add(5);
-    ej.show();
+    Clase1<double> ej;
+    ej.add(2.3);
+    ej.add(5.5);
+    vector<string> new_v = ej.procesar();
+    for(auto val: new_v){
+        cout << val << typeid(val).name()<<endl;
+    }
+    Clase1<string> ej2;
+    ej2.add("Hola");
+    ej2.add("mundo");
+     vector<string> new_v2 = ej2.procesar();
+    for(auto val: new_v2){
+        cout << val << typeid(val).name()<<endl;
+    }
+    Clase1<vector<int>> ej3;
+    vector<int> a = {1,2}
+    vector<int> b = {3,4}
+    ej2.add();
+    ej2.add("mundo");
+     vector<string> new_v2 = ej2.procesar();
+    for(auto val: new_v2){
+        cout << val << typeid(val).name()<<endl;
+    }
 }
